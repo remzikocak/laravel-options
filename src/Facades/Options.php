abf1dddd1368e683d5e8f6a4be65ec0a41564adf
Remzi <me@remzikocak.com>
2020-07-18 00:06:03 +0300

    Add doc block

--- src/Facades/Options.php
+++ src/Facades/Options.php
@@ -5,7 +5,16 @@ namespace RKocak\Options\Facades;
 
 
 use Illuminate\Support\Facades\Facade;
+use RKocak\Options\Loader;
+use RKocak\Options\Types;
 
+/**
+ * @method static mixed get(string $key, $default = null)
+ * @method static bool has(string $key)
+ * @method static Types getTypes()
+ * @method static Loader getLoader()
+ * @method static void load()
+ */
 class Options extends Facade
 {
 
